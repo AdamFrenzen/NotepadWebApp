13007f56a52210d9fa227bef656ba71f789c5ca9
AdamFrenzen <adam.frenzen@gmail.com>
2023-10-31 19:58:12 -0600

    Sidebar and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NotesListComponent } from './notes-list/notes-list.component';
 import { NoteWritingAreaComponent } from './note-writing-area/note-writing-area.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesListComponent,
-    NoteWritingAreaComponent
+    NoteWritingAreaComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
--- src/app/notes-list/notes-list.component.ts
+++ src/app/notes-list/notes-list.component.ts
@@ -6,9 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./notes-list.component.css']
 })
 export class NotesListComponent {
+
+  public selected: number | null = 1
+
   notes = [{
     name: 'note1'
   }, {
     name: 'note2'
-  }]
+  }, {
+    name: 'note3'
+  }, {
+    name: 'note4'
+  },]
 }
